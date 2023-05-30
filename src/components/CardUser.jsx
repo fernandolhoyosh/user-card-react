d6588099790fe634c02151d91b02e1926b4f79ea
Fernando Hoyos <fernandolhoyosh@gmail.com>
2023-05-30 10:19:23 -0500

    Estructura final tarjeta de usuario

--- src/components/CardUser.jsx
+++ src/components/CardUser.jsx
@@ -1,16 +1,18 @@
-import './CardUser.css'
+import "./CardUser.css";
 import { FcBusinessman } from "react-icons/fc";
 
-function CardUser (){
-    return(
-        <div className="target-card">
-            <h5>Sergio</h5>
-            <picture><FcBusinessman/></picture>
-            <p>Foto de perfil</p>
-            <p>23</p>
-            <button>Take a car</button>
-        </div>
-    )
+function CardUser() {
+  return (
+    <div className="target-card">
+      <h4>Sergio</h4>
+      <picture className="background-photo">
+        <FcBusinessman id="photo-user" />
+      </picture>
+      <p>Foto de perfil</p>
+      <p>23</p>
+      <button>Take a car</button>
+    </div>
+  );
 }
 
-export {CardUser}
+export { CardUser };
